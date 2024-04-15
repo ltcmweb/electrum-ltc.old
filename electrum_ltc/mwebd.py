7c2d3c2398de9080a0e46ff9f626ca97cecaa82d
Hector Chu <hectorchu@gmail.com>
2024-04-15 09:11:58 +0100

    Remove unused arg

--- electrum_ltc/mwebd.py
+++ electrum_ltc/mwebd.py
@@ -37,8 +37,7 @@ def _start_if_needed():
             data_dir = os.path.join(data_dir, network)
         data_dir = os.path.join(data_dir, 'mweb')
         Path(data_dir).mkdir(parents=True, exist_ok=True)
-        args = ['./mwebd', '-c', network, '-d', data_dir,
-                '-l', str(port), '-ppid', str(os.getpid())]
+        args = ['./mwebd', '-c', network, '-d', data_dir, '-l', str(port)]
         process = subprocess.Popen(args, stdout=subprocess.DEVNULL)
 
         while True:
